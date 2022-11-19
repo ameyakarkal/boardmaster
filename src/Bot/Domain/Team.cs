8ae261e00a30f324dd0020c80f961171a1e777ab
akarkal <akarkal@anthology.com>
2022-11-19 18:07:34 -0500

    state

--- src/Bot/Domain/Team.cs
+++ src/Bot/Domain/Team.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Bot.Domain;
 
@@ -6,11 +7,16 @@ public class Team
 {
     static Team()
     {
-        TeamMembers = new List<TeamMember>
-        {
-            new TeamMember("Bob")
-        };
+        TeamMembers = SuicideSquad().ToList();
     }
 
     public static IList<TeamMember> TeamMembers { get; }
+
+    private static IEnumerable<TeamMember> SuicideSquad()
+    {
+        yield return new TeamMember("Ryan Fiust-Klink");
+        yield return new TeamMember("George Pineda");
+        yield return new TeamMember("Cody Pritchett");
+        yield return new TeamMember("David Meyers");
+    }
 }
--- src/Bot/Domain/TeamMember.cs
+++ src/Bot/Domain/TeamMember.cs
@@ -1,3 +1,6 @@
 namespace Bot.Domain;
 
-public record TeamMember(string Name);
+
+public record TeamMember(
+    string Name, 
+    string AboutMarkDown = "Apparently, this user prefers to keep an air of mystery about them.");
